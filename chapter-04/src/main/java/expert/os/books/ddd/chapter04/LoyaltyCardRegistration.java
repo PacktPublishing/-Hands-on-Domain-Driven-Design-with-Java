28fd373c39e58b77f278e81b8c89b80b9ced8eb0
Otavio Santana <otaviopolianasantana@gmail.com>
2024-10-01 19:45:50 +0100

    chore: update card structure
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

---
+++ chapter-04/src/main/java/expert/os/books/ddd/chapter04/LoyaltyCardRegistration.java
@@ -0,0 +1,17 @@
+package expert.os.books.ddd.chapter04;
+
+import java.time.LocalDate;
+
+public class LoyaltyCardRegistration {
+    private final String id;
+    private final Customer customer;
+    private final LocalDate registrationDate;
+    private CardStatus status;
+
+    public LoyaltyCardRegistration(String id, Customer customer, LocalDate registrationDate) {
+        this.id = id;
+        this.customer = customer;
+        this.registrationDate = registrationDate;
+        this.status = CardStatus.ACTIVE;  // Default status upon registration
+    }
+}
