f5d1c3e35457c101dbeff66c104e9d10d08a87d7
Otavio Santana <otaviopolianasantana@gmail.com>
2024-10-19 17:32:24 +0100

    test: create test scenarios to hotelmapper
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- chapter-05/src/main/java/expert/os/books/ddd/chapter04/hotels/application/HotelMapper.java
+++ chapter-05/src/main/java/expert/os/books/ddd/chapter04/hotels/application/HotelMapper.java
@@ -2,8 +2,13 @@ package expert.os.books.ddd.chapter04.hotels.application;
 
 import expert.os.books.ddd.chapter04.hotels.Guest;
 import expert.os.books.ddd.chapter04.hotels.Room;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
 
+@Mapper
 public interface HotelMapper  {
+
+    HotelMapper INSTANCE = Mappers.getMapper( HotelMapper.class );
 
     RoomDTO toDTO(Room room);
 
