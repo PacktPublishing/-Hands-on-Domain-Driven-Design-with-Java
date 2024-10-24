924d273f02bdacbdc6a2c08789fdee3935fe87e5
Otavio Santana <otaviopolianasantana@gmail.com>
2024-10-24 04:34:12 +0100

    feat: create modules to port and adapter
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- chapter-06/src/main/java/expert/os/books/ddd/chapter06/hotels/controller/HotelController.java
+++ chapter-06/src/main/java/expert/os/books/ddd/chapter06/hotels/controller/HotelController.java
@@ -1,28 +1,32 @@
 package expert.os.books.ddd.chapter06.hotels.controller;
 
-import expert.os.books.ddd.chapter06.hotels.HotelService;
+import expert.os.books.ddd.chapter06.hotels.CheckInUseCase;
+import expert.os.books.ddd.chapter06.hotels.CheckOutUseCase;
 import expert.os.books.ddd.chapter06.hotels.Room;
+import org.jmolecules.architecture.hexagonal.Port;
 
 //Here we hava either a controller or a resource to expose the application services
+@Port
 public class HotelController {
 
-    private final HotelService hotelService;
-
+    private final CheckInUseCase checkInUseCase;
+    private final CheckOutUseCase checkOutUseCase;
     private final HotelMapper hotelMapper;
 
-    public HotelController(HotelService hotelService, HotelMapper hotelMapper) {
-        this.hotelService = hotelService;
+    public HotelController(CheckInUseCase checkInUseCase, CheckOutUseCase checkOutUseCase, HotelMapper hotelMapper) {
+        this.checkInUseCase = checkInUseCase;
+        this.checkOutUseCase = checkOutUseCase;
         this.hotelMapper = hotelMapper;
     }
 
     public RoomDTO checkInRoom(GuestDTO guestDTO) {
         var guest = hotelMapper.toEntity(guestDTO);
-        Room room = hotelService.checkIn(guest);
+        Room room = checkInUseCase.checkIn(guest);
         return hotelMapper.toDTO(room);
     }
 
     public void checkOutRoom(RoomDTO roomDTO) {
         var room = hotelMapper.toEntity(roomDTO);
-        hotelService.checkOut(room);
+        checkOutUseCase.checkOut(room);
     }
 }
