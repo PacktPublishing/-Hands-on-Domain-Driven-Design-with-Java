e22922579eab38d7d90d6ff8061b174c6ef5846b
Otavio Santana <otaviopolianasantana@gmail.com>
2024-11-09 06:18:50 +0000

    feat: hide implementation to jpa
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- chapter-07/src/main/java/expert/os/books/ddd/chapter07/hotels/jpa/JPAMapper.java
+++ chapter-07/src/main/java/expert/os/books/ddd/chapter07/hotels/jpa/JPAMapper.java
@@ -3,10 +3,9 @@ package expert.os.books.ddd.chapter07.hotels.jpa;
 import expert.os.books.ddd.chapter07.hotels.Guest;
 import expert.os.books.ddd.chapter07.hotels.Room;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
 
 @Mapper
-public interface JPAMapper {
+interface JPAMapper {
 
     GuestJPA toEntity(Guest guest);
 
