7ef4b918ef162fee61ae6b270bc0e95baa283849
Otavio Santana <otaviopolianasantana@gmail.com>
2024-11-10 18:57:55 +0000

    style: remove imports
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- chapter-09/src/test/java/expert/os/books/ddd/chapter09/hotels/RoomServiceTest.java
+++ chapter-09/src/test/java/expert/os/books/ddd/chapter09/hotels/RoomServiceTest.java
@@ -1,10 +1,10 @@
 package expert.os.books.ddd.chapter09.hotels;
+
 import jakarta.persistence.EntityNotFoundException;
 import org.assertj.core.api.SoftAssertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.transaction.annotation.Transactional;
