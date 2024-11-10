eeaae5362fb03ce5b19d104d92894148e7d352ba
Otavio Santana <otaviopolianasantana@gmail.com>
2024-11-10 08:25:32 +0000

    style: enhance imports at HotelController
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- chapter-09/src/main/java/expert/os/books/ddd/chapter09/hotels/HotelController.java
+++ chapter-09/src/main/java/expert/os/books/ddd/chapter09/hotels/HotelController.java
@@ -2,7 +2,6 @@ package expert.os.books.ddd.chapter09.hotels;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
